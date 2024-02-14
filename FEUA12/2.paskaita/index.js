6204335a62a7baa05de64ef555ed3746ea8c2fc3
GintarėAndr <gintaret18@gmail.com>
2024-02-14 20:27:07 +0200

    add get post put and del

--- FEUA12/2.paskaita/index.js
+++ FEUA12/2.paskaita/index.js
@@ -9,13 +9,37 @@ app.get("/",(req, res) => {
     res.send("Sklandziai");
 });
 
-const brand = ["Toyota", "Audi", "BMV"];
+const brand = ["Beantley", "Audi", "BMV"];
 
 app.get("/brand", (req, res) => {
     res.send(brand);
 
 });
 
+// /brands/B => ["BMV"];
+// /brands/g => [];
+// /brands/b =>
+// dinaminis route
+app.get("/brands/:firstLetter", (req, res) => {
+    // grazinti visus brandus kurie prasideda duota raide
+    // const firstLetter = req.params.firstLetter; old way/another way
+    const { firstLetter } = req.params; // su dvitaskiu einantys parameterai;
+    console.log(firstLetter);
+  
+    const filteredBrands = brands.filter(
+      (brand) => brand[0].toLowerCase() === firstLetter.toLowerCase()
+    );
+  
+    res.send(filteredBrands);
+  });
+  
+  // POST API kuris prideda brandą į masyvą
+  app.post("/brands", (req, res) => {
+    const { brand } = req.body; // siunciamas body is frontend, butinai turi but objektas ar masyvas
+    brands.push(brand);
+    res.send(brands);
+  });
+  
 
 
 
@@ -23,3 +47,4 @@ app.get("/brand", (req, res) => {
 app.listen(port,() => {
     console.log(`App is listening  on the port ${port}`);
 }); 
+
